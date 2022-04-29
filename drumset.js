c3230d1bfbfe12844f2c91f6af99a023680b682b
Shruti-kothiyal <53954275+Shruti-kothiyal@users.noreply.github.com>
2022-04-29 13:46:36 +0530

    Add files via upload

---
+++ drumset.js
@@ -0,0 +1,45 @@
+var clap=document.getElementById('clap');
+var audio_clap=new Audio("/Drumset Audio Files/clap.wav")
+clap.addEventListener('click',function(){
+    audio_clap.play();
+})
+var hihat=document.getElementById('hihat');
+var audio_hihat=new Audio("/Drumset Audio Files/hihat.wav")
+hihat.addEventListener('click',function(){
+    audio_hihat.play();
+})
+var kick=document.getElementById('kick');
+var audio_kick=new Audio("/Drumset Audio Files/kick.wav")
+kick.addEventListener('click',function(){
+    audio_kick.play();
+})
+var openhat=document.getElementById('openhat');
+var audio_openhat=new Audio("/Drumset Audio Files/openhat.wav")
+openhat.addEventListener('click',function(){
+    audio_openhat.play();
+})
+var boom=document.getElementById('boom');
+var audio_boom=new Audio("/Drumset Audio Files/boom.wav")
+boom.addEventListener('click',function(){
+    audio_boom.play();
+})
+var ride=document.getElementById('ride');
+var audio_ride=new Audio("/Drumset Audio Files/ride.wav")
+ride.addEventListener('click',function(){
+    audio_ride.play();
+})
+var snare=document.getElementById('snare');
+var audio_snare=new Audio("/Drumset Audio Files/snare.wav")
+snare.addEventListener('click',function(){
+    audio_snare.play();
+})
+var tom=document.getElementById('tom');
+var audio_tom=new Audio("/Drumset Audio Files/tom.wav")
+tom.addEventListener('click',function(){
+    audio_tom.play();
+})
+var tink=document.getElementById('tink');
+var audio_tink=new Audio("/Drumset Audio Files/tink.wav")
+tink.addEventListener('click',function(){
+    audio_tink.play();
+})
